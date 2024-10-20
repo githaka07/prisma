a057a418c5719484b56c0895bd1b878629b65fc5
githaka07 <githaka07@gmail.com>
2024-10-20 13:35:53 +0300

    feat(ui): Buttion
    Created Button component, registered it, styled it & its styles and mounted it.

--- app/ui/Button/Button.tsx
+++ app/ui/Button/Button.tsx
@@ -1,5 +1,13 @@
+import { ButtonStyles } from '@styles';
 import Link from 'next/link';
 
-export default function Button({ URL, name }) {
-    return <Link href={URL}>{name}</Link>;
+export default function Button({ URL, name }: ButtonProps) {
+    return (
+        <Link
+            className={ButtonStyles.Button}
+            href={URL}
+        >
+            {name}
+        </Link>
+    );
 }
