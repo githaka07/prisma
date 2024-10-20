575c161cc71e238ca84c428ae5e0b8c7899332c7
githaka07 <githaka07@gmail.com>
2024-10-20 09:51:32 +0300

    feat: Metadata
    Added Home page metadata.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import { Header, Main } from '@layout';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Prisma',
+};
 
 export default function Home() {
     return (
